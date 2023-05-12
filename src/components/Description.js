299f99ddd8ec73a71dcac16ec084e5cc2738124c
Alextest2 <olepry98@gmail.com>
2023-05-13 00:35:43 +0300

    Added description

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ function Description() {
   return (
     <div className=' w-screen flex flex-col items-center justify-center bg-cover h-500 xl:h-650 xl:w-1170 xl:bg-xl lg:bg-lg  md:bg-md  sm:bg-sm '>
       <h1 className=' text-heading text-center text-white w-380 sm:w-328 sm:whitespace-break-spaces whitespace-pre-wrap'>Test assignment for front-end developer</h1>
-      <p className='text-body text-center text-white w-380 sm:w-328 mt-21 whitespace-break-spaces whitespace-pre-wrap'>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving. </p>
+      <p className='text-body text-center text-white w-380 sm:w-328 mt-21 sm:whitespace-break-spaces whitespace-pre-wrap'>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving. </p>
       <Button
         spacing='mt-8'
         type='button'
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import Logo from "../img/logo.svg";
 import Button from "./ui/Button";
